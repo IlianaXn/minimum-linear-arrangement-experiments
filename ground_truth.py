a3339c8ba80b5a76d69dac9af144858c18e7f4b4
IlianaXn <el17059@mail.ntua.gr>
2022-02-25 16:30:47 +0200

    Add exact algorithm of mla

---
+++ ground_truth.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 25 10:40:46 2022
+
+@author: ilmar
+"""
+import laldebug as lal
+
+# create graph
+football = lal.io.read_edge_list('free_tree', 'football.txt')
+
+# compute minimum linear arrangement and its sum using Chung's algorithm
+min_sum, _ = lal.linarr.min_sum_edge_lengths(football, lal.linarr.algorithms_Dmin.Chung_2)
+print('Sum of minimum linear arrangement =', min_sum)
+
+# help(lal.types.linear_arrangement.from_direct)
